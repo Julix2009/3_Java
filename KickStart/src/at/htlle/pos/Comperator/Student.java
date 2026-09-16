7ddcd54ae09ee35768fb03bb2a4c61fa6b6d7827
Julix2009 <Massong.Julian@proton.me>
2026-09-16 12:38:43 +0200

    Changes in StudenT class

--- KickStart/src/at/htlle/pos/Comperator/Student.java
+++ KickStart/src/at/htlle/pos/Comperator/Student.java
@@ -1,8 +1,21 @@
 package at.htlle.pos.Comperator;
 
+import java.util.Objects;
+
 public class Student {
     private String classroom;
     private String name;
     private int id;
+
+    public Student (  int id, String classroom, String name){
+        if(id < 0) throw new IllegalArgumentException("Passed argument id  must be over null");
+        this.id = id;
+
+        this.name = Objects.requireNonNull(name, " Passed argument must not be null");
+
+        this.classroom = classroom;
+
+
+    }
 
 }
