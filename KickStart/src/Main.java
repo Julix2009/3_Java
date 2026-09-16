9586bdeba7ef4c0c40a641cb5d6864ce27ff9c8d
Julix2009 <Massong.Julian@proton.me>
2026-09-16 11:46:01 +0200

    FirstCommit

---
+++ KickStart/src/Main.java
@@ -0,0 +1,40 @@
+void main(){
+    // SHIFT + Alt move lines up and down
+    // CTRL+y delet line
+    // dtrl +  D dublicate line
+    List<Integer> myList = new ArrayList <>();
+
+    for (int i = 0;  i< 10; i++) {
+        myList.add(i); //Auto Boxing :)
+
+    }
+
+    for(Integer i : myList){ // auto unboxing
+        System.out.println(i); //sout
+    }
+    System.out.println(myList.get(5));
+    myList.set(5,67);
+
+
+    for(Integer i : myList){ // auto unboxing
+        System.out.println(i); //sout
+
+    }
+
+    for (int i = 0; i < myList.size(); i++) {
+        int temp = myList.get(i);
+        myList.set(i, temp + 120);
+
+    }
+
+    System.out.println("Verändert:");
+
+    for(Integer i : myList){ // auto unboxing
+        System.out.print(i+ ","); //sout
+
+    }
+
+
+
+
+        }
