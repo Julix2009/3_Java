94d9eac96206eceadc8712f91793bd1bd9800625
Julix2009 <Massong.Julian@proton.me>
2026-09-25 09:04:22 +0200

    added cases

--- 02_MavenPlayground/src/main/java/at/htlle/pos/pcketcalcmain.java
+++ 02_MavenPlayground/src/main/java/at/htlle/pos/pcketcalcmain.java
@@ -2,12 +2,63 @@ package at.htlle.pos;
 
 public class pcketcalcmain {
     static void main(){
-         int result = pocketCalculator.addition(1,2);
-         int expectet = 3;
+         long result = pocketCalculator.addition(1,2);
+         long expectet = 3;
          String testresult;
          if(result==expectet) testresult = "ok";
          else testresult = "failed";
         System.out.printf("Pocketcalculator.addition(1,2) expectet result = 3 , actural =%d, test result: %s",result,testresult);//3
-        System.out.println(pocketCalculator.subtraction(1,2)); //-1
+        // cases skipped;
+        // result 0;
+        //0,0;
+
+        System.out.println("----/n");
+        //aString testresult;ddilong result = pocketCalculator.addition(1,2);tion with overflow
+        result = pocketCalculator.addition(Integer.MAX_VALUE,1);
+        expectet = 2147483648L;
+        if(result==expectet) testresult = "ok";
+        else testresult = "failed";
+
+        System.out.printf("Pocketcalculator.addition(1,2) expectet result = 2147483648L  , actural =%d, test result: %s",result,testresult);
+
+        System.out.println("----/n");
+        System.out.println("Adding negative numbers");
+        // -1 , 3 --> positive result
+        //1, -2 --> negative results
+        // 1 , -2 --> negative result
+        // -2 , 4 --> neg. result
+        // 1 , -1 --> 0
+        //underflow
+        //Interger.Min_VALUE, -1
+
+        System.out.println("---/n");
+
+        expectet = 1;
+        result = pocketCalculator.addition(-1, 2);
+
+        if(result==expectet) testresult = "ok";
+        else testresult = "failed";
+
+        System.out.printf("Pocketcalculator.addition(-1,2) expectet result = 1   , actural =%d, test result: %s",result,testresult);
+
+        System.out.println("---- /n");
+
+        expectet = 0;
+        result = pocketCalculator.addition(1, -1);
+
+        if(result==expectet) testresult = "ok";
+        else testresult = "failed";
+
+        System.out.printf("Pocketcalculator.addition(1,-1) expectet result = 1   , actural =%d, test result: %s",result,testresult);
+
+
+
+
+
+
+
+
+
+
     }
 }
