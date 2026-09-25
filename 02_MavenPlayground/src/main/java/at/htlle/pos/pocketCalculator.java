35ba72ccace62de7ba53e70ce28b8df127247916
Julix2009 <Massong.Julian@proton.me>
2026-09-25 08:54:08 +0200

    cast to float

--- 02_MavenPlayground/src/main/java/at/htlle/pos/pocketCalculator.java
+++ 02_MavenPlayground/src/main/java/at/htlle/pos/pocketCalculator.java
@@ -5,8 +5,8 @@ public class pocketCalculator {
     //basic claculator
     //+, -, *, /
 
-    public static int  addition(int numb1, int numb2){
-        return numb1 + numb2;
+    public static long addition(int numb1, int numb2){
+        return (long)numb1 + numb2;
     }
 
     public static int  subtraction(int numb1, int numb2){
