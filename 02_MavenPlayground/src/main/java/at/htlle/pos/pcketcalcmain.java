0de16753bea6db1d69b121484bf4a24d2de5054a
Julix2009 <Massong.Julian@proton.me>
2026-09-25 08:37:49 +0200

    testresult addition added

--- 02_MavenPlayground/src/main/java/at/htlle/pos/pcketcalcmain.java
+++ 02_MavenPlayground/src/main/java/at/htlle/pos/pcketcalcmain.java
@@ -2,7 +2,12 @@ package at.htlle.pos;
 
 public class pcketcalcmain {
     static void main(){
-        System.out.println(pocketCalculator.addition(1,2));//3
+         int result = pocketCalculator.addition(1,2);
+         int expectet = 3;
+         String testresult;
+         if(result==expectet) testresult = "ok";
+         else testresult = "failed";
+        System.out.printf("Pocketcalculator.addition(1,2) expectet result = 3 , actural =%d, test result: %s",result,testresult);//3
         System.out.println(pocketCalculator.subtraction(1,2)); //-1
     }
 }
