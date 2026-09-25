42fa2a4995feff63c044a4b59a93fc071c9b79f4
Julix2009 <Massong.Julian@proton.me>
2026-09-25 08:31:49 +0200

    Calculator

---
+++ 02_MavenPlayground/src/main/java/at/htlle/pos/pcketcalcmain.java
@@ -0,0 +1,8 @@
+package at.htlle.pos;
+
+public class pcketcalcmain {
+    static void main(){
+        System.out.println(pocketCalculator.addition(1,2));//3
+        System.out.println(pocketCalculator.subtraction(1,2)); //-1
+    }
+}
---
+++ 02_MavenPlayground/src/main/java/at/htlle/pos/pocketCalculator.java
@@ -0,0 +1,25 @@
+package at.htlle.pos;
+//tool class to provide functionality to compute basic stuff
+
+public class pocketCalculator {
+    //basic claculator
+    //+, -, *, /
+
+    public static int  addition(int numb1, int numb2){
+        return numb1 + numb2;
+    }
+
+    public static int  subtraction(int numb1, int numb2){
+        return numb1 - numb2;
+    }
+
+    public static int multiplication(int numb1, int numb2){
+        return numb1 * numb2;
+    }
+
+    public static int division(int numb1, int numb2){
+        return numb1 / numb2;
+    }
+
+
+}
